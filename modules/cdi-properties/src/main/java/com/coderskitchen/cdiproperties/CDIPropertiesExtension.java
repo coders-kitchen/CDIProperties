e178c5035a572e1610afb39af52d628540d87d65
Peter Daum <peter.daum.80@gmail.com>
2014-02-23 13:49:12 +0100

    Added license headers

--- modules/cdi-properties/src/main/java/com/coderskitchen/cdiproperties/CDIPropertiesExtension.java
+++ modules/cdi-properties/src/main/java/com/coderskitchen/cdiproperties/CDIPropertiesExtension.java
@@ -1,3 +1,25 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Peter Daum
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package com.coderskitchen.cdiproperties;
 
 import javax.enterprise.event.Observes;
@@ -35,17 +57,20 @@ import java.util.Set;
  * These properties can now injected as shown in the following class Author
  * <pre>
  *
- * @PropertyFile("example.properties") public class Author {
- * @Property("author"); private String author;
- * @Property("country") private String country;
- * <p/>
- * public String getAuthor() {
- * return author;
- * }
- * <p/>
- * public String getCountry() {
- * return country;
- * }
+ * {@literal @}PropertyFile("example.properties")
+ * public class Author {
+ *   {@literal @}Property("author");
+ *   private String author;
+ *   {@literal @}Property("country")
+ *   private String country;
+ *
+ *   public String getAuthor() {
+ *     return author;
+ *   }
+ *
+ *   public String getCountry() {
+ *     return country;
+ *   }
  * }
  * </pre>
  */
